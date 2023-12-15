88f02843dd0fd4758f1ed34340be7c77c7179bc8
marwa-em <marwaedwin94@gmail.com>
2023-12-15 05:26:09 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - this prints exactly the input on it 
- * Return: 0 always 1
+ *main - this prints exactly the input on it
+ *Return: 0 always 1
  */
 int main(void)
 {
-	write (2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
 }
 
