965bb05e78fc16e49342f1ff406fdbf46b73157f
marwa-em <marwaedwin94@gmail.com>
2023-12-15 02:33:38 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - this subscribes the main function of the code
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(d);
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(e));
+	return (0);
+	}
+
