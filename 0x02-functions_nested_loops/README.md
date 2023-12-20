3c914dfd176d5e222bf28aa61bdc7ed9f4bd69e7
marwa-em <marwaedwin94@gmail.com>
2023-12-20 07:32:37 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+my other nested loop
---
+++ 0x02-functions_nested_loops/ascii.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- this is the main  ascii function
+ *Return:always 0
+ */
+int main(void)
+{
+char ch = 'A';
+printf("value of %c is %d\n,", ch, ch);
+return (0);
+}
