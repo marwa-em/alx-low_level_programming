d4b6b3d98999155302c70d1821294715a862230a
marwa-em <marwaedwin94@gmail.com>
2023-12-21 09:59:29 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -15,13 +15,15 @@ unsigned long int term2 = 2;
 unsigned long int next;
 int i;
 
-printf("%lu", term1);
+printf("%lu, ", term1);
 for (i = 1; i < 50; i++)
 {
 printf("%lu", term2);
 next = term1 + term2;
 term1 = term2;
 term2 = next;
+if (i != 49)
+printf(", ");
 }
 printf("\n");
 
