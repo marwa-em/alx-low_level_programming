c554ae8d438c6ed5be892118fd2d9f75712af486
marwa-em <marwaedwin94@gmail.com>
2023-12-21 09:35:10 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,51 +2,29 @@
 
 
 /**
- *main _ The main function
+ *main -  The main function
  *1 and 2, seperated by a comma
  *Return: 0.
  */
 
 
 int main(void)
-
-
 {
-int count;
-unsigned long fib1 = 0, fib2 = 1, sum;
-unsigned long fib1_half1, fib2_half1, fib2_half2;
-unsigned long half1, half2;
+int term1 = 1;
+int term2 = 2;
+int next;
+int i;
 
-
-for (count = 0; count < 92; count++)
+printf("%d", term1);
+for (i = 1; i < 50; i++)
 {
-sum = fib1 + fib2;
-printf("%lu, ", sum);
-fib1 = fib2;
-fib2 = sum;
-}
-fib1_half1 = fib1 / 10000000000;
-fib2_half1 = fib2 / 10000000000;
-fib1_half2 = fib1 % 10000000000;
-fib2_half2 = fib2 % 10000000000;
-for (count = 93; count < 99; count++)
-{
-half1 = fib1_half1 + fib2_half1;
-half2 = fib1_half2 + fib2_half2;
-if (fib1_half2 + fib2_half2 > 9999999999)
-{
-half1 += 1;
-half2 %= 10000000000;
-}
-printf("%lu%lu", half1, half2);
-if (count != 98)
-printf(", ");
-fib1_half1 = fib2_half1;
-fib1_half2 = fib2_half2;
-fib2_half1 = half1;
-fib2_half2 = half2;
+printf("%d", term2);
+next = term1 + term2;
+term1 = term2;
+term2 = next;
 }
 printf("\n");
+
 return (0);
 }
 
