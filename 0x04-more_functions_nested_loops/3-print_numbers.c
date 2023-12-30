ae0fbe7bd96fe291ce19b35133db5bbcc59f62f6
marwa-em <marwaedwin94@gmail.com>
2023-12-30 08:04:52 -0800

    _putchar.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,13 +7,14 @@
 
 void print_numbers(void)
 {
-char c;
+	char c;
 
-for (c = '0'; c <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
 
-_putchar(c);
-
-_putchar('\n');
+	_putchar('\n');
+	}
 }
 
 
