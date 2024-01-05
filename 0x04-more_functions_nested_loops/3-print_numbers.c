8b1a4548d8dda1e9600fda419c6ae3b9e25a188a
marwa-em <marwaedwin94@gmail.com>
2024-01-04 22:54:07 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  *print_numbers - print 0-9
- *Return: void
+ *return: void
  */
 
 void print_numbers(void)
@@ -11,10 +11,10 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	_putchar('c');
-
-	_putchar('\n');
+	_putchar(c);
 	}
+	_putchar('\n');
 }
+
 
 
