c19981fd559ca101afec719a885f29de3693472a
marwa-em <marwaedwin94@gmail.com>
2024-01-07 15:20:04 -0800

    8-print_array.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void print_array(int *a, int n);
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -11,5 +12,6 @@ void rev_string(char *s);
 void puts2(char *str);
 char *_strcpy(char *dest, char *src);
 void puts_half(char *str);
+
 
 #endif
