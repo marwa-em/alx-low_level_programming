b1ac75fa5a5e9328083fd1e88639c989a0d455a2
marwa-em <marwaedwin94@gmail.com>
2024-01-08 08:25:46 -0800

    0-preprocessor

---
+++ 0x00-hello_world/while.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+*
+*main - this is for while loop
+*
+*Return: 0
+*/
+
+int main(void)
+
+{
+
+char  i = 'a';
+
+while (i = || < 'z')
+{
+printf("%c\n", i);
+
+i++;
+}
+return (0);
+}
