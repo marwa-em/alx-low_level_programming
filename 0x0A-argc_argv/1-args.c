62d65d6d1635052cc7093469b0a8230f0da4626c
marwa-em <marwaedwin94@gmail.com>
2024-01-18 00:32:39 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main- prints the number of argiments
+ *@argc: counts the number of arguments
+ *@argv: argument vector
+ *Return: always 0
+ *
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
+
